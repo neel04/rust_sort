89066061eaab15ba861ebf6e4ae38fa0ad9dded0
Neel Gupta <neelgupta04@outlook.com>
2024-01-31 17:05:53 +0000

    Added traits

--- src/algos.rs
+++ src/algos.rs
@@ -1,19 +1,25 @@
-use std::vec::Vec;
+pub trait Sorter {
+    fn sort(&self, v: &[i32]) -> Vec<i32>;
+}
 
-pub fn bubble_sort(v: &Vec<i32>) -> Vec<i32> {
-    /* 
-    Implements bubble sort algorithm
-    */
-    let mut sorted_vec = v.clone();
-    let vec_length = sorted_vec.len();
+pub struct BubbleSorter;
 
-    for i in 0..vec_length-1 {
-        for j in 0..vec_length-i-1 {
-            if sorted_vec[j] > sorted_vec[j+1] {
-                sorted_vec.swap(j, j+1);
+impl Sorter for BubbleSorter {
+    fn sort(&self, v: &[i32]) -> Vec<i32> {
+        /* 
+        Implements bubble sort algorithm
+        */
+        let mut sorted_vec = v.to_vec();
+        let vec_length = sorted_vec.len();
+
+        for i in 0..vec_length - 1 {
+            for j in 0..vec_length - i - 1 {
+                if sorted_vec[j] > sorted_vec[j+1] {
+                    sorted_vec.swap(j, j+1);
+                }
             }
         }
-    }
 
-    sorted_vec
+        sorted_vec
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-// use algos.rs which is placed in src/algos.rs
-use crate::algos::bubble_sort;
+// Get Sorter trait
+use algos::Sorter;
 
 // Declare public modules
 pub mod algos;
 
-fn run_algo(vec: &Vec<i32>, sorting_fn: fn(&Vec<i32>) -> Vec<i32>, fn_name: String) -> Vec<i32> {
+fn run_algo(vec: &[i32], sorter: &dyn Sorter, fn_name: &str) -> Vec<i32> {
+    let sorted_vec = sorter.sort(vec);
+
     println!("---- {} Sort ----", fn_name);
     println!("Unsorted array: {:?}", &vec);
-    let sorted_vec = sorting_fn(vec);
     println!("Sorted array: {:?}", &sorted_vec);
+
     sorted_vec
 }
 
 fn main() {
-    let my_vec = vec![10, 3, 5, 2, 1, 0, 4, 9, 8, 7, 6];
-    let _sorted_vec = run_algo(&my_vec, bubble_sort, "Bubble".to_string());
+    let my_slice = &[3, 5, 2, 1, 0, 4, 9, 8, 7, 6];
+
+    // Get all the algos
+    let bubble_sort = algos::BubbleSorter;
+
+    // Run the algos
+    let _ = run_algo(my_slice, &bubble_sort, "Bubble");
 }
